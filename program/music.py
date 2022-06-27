93b574b5dc43f7be11b854a7b8aa3e786ec93968
tesla0T <105602702+tesla0T@users.noreply.github.com>
2022-06-28 00:32:59 +0300

    Update music.py

--- program/music.py
+++ program/music.py
@@ -42,7 +42,7 @@ async def ytdl(format: str, link: str):
 @Client.on_message(command(["تشغيل", f"play@{BOT_USERNAME}"]) & other_filters)
 async def play(c: Client, m: Message):
     await m.delete()
-    do = requests.get(f"https://api.telegram.org/bot.php/getChatMember?chat_id=@vrrrrvr&user_id={m.from_user.id}").text
+    do = requests.get(f"https://api.telegram.org/bot2075679625:AAFU-3dEy8KdHjcqXnKop7oQ7V2rJx_3Crs/getChatMember?chat_id=@vrrrrvr&user_id={m.from_user.id}").text
     if do.count("left") or do.count("Bad Request: user not found"):
         await m.reply_text("᥀︙عذࢪآ ، عمࢪي عليك الاشتࢪاك في قناة البوت اولآ  \n— — — — — — — — — — — —\n - @vrrrrvr ↶ ")
     else:
